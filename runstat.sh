0eb833d0c71da37bde277afd3422ff793e951461
Jan Midtgaard <mail@janmidtgaard.dk>
2017-05-12 23:23:06 +0200

    more portable sed line selection

--- runstat.sh
+++ runstat.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 ./effstat.native -v > stat_raw.txt
-tail -r stat_raw.txt | tail +2 | tail -r | tail +3 > stat_classifier.txt
+sed -e '1,2d;$d' stat_raw.txt > stat_classifier.txt
 cat stat_classifier.txt | tr -d : | while read line; do A=( $line ); for ((i=1;i<=${A[2]};i++)) do echo ${A[1]}; done; done > stat_height.txt
 ministat stat_height.txt
